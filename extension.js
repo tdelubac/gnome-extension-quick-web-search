840504f875eded5bd14aa1ab17e547ea2e6268b0
Timothée Delubac <timothee.delubac@protonmail.com>
2023-06-10 18:47:03 +0300

    Duckduckgo as default

--- extension.js
+++ extension.js
@@ -18,17 +18,219 @@
 
 /* exported init */
 
-class Extension {
-    constructor() {
+const Main = imports.ui.main;
+const { St, Gio } = imports.gi;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+let gicon = Gio.icon_new_for_string(Me.path + "/internet-web-browser.svg");
+
+const SearchResults = Main.overview._overview._controls._searchController._searchResults;
+
+
+class SearchProvider {
+
+    /**
+     * The application of the provider.
+     *
+     * Applications will return a `Gio.AppInfo` representing themselves.
+     * Extensions will usually return `null`.
+     *
+     * @type {Gio.AppInfo}
+     */
+    get appInfo() {
+        return null;
     }
 
-    enable() {
+    /**
+     * Whether the provider offers detailed results.
+     *
+     * Applications will return `true` if they have a way to display more
+     * detailed or complete results. Extensions will usually return `false`.
+     *
+     * @type {boolean}
+     */
+    get canLaunchSearch() {
+        return true;
     }
 
-    disable() {
+    /**
+     * The unique ID of the provider.
+     *
+     * Applications will return their application ID. Extensions will usually
+     * return their UUID.
+     *
+     * @type {string}
+     */
+    get id() {
+        return imports.misc.extensionUtils.getCurrentExtension().uuid;
+    }
+
+    /**
+     * Launch the search result.
+     *
+     * This method is called when a search provider result is activated.
+     *
+     * @param {string} result - The result identifier
+     * @param {string[]} terms - The search terms
+     */
+    activateResult(result, terms) {
+        let context = new Gio.AppLaunchContext;
+//        Gio.AppInfo.launch_default_for_uri('www.lemonde.fr', context);
+        Gio.AppInfo.create_from_commandline(`xdg-open "https://www.duckduckgo.com/?q=${terms.join('+')}"`, null, 2).launch([], context);
+    }
+
+    /**
+     * Launch the search provider.
+     *
+     * This method is called when a search provider is activated. A provider can
+     * only be activated if the `appInfo` property holds a valid `Gio.AppInfo`
+     * and the `canLaunchSearch` property is `true`.
+     *
+     * Applications will typically open a window to display more detailed or
+     * complete results.
+     *
+     * @param {string[]} terms - The search terms
+     */
+    launchSearch(terms) {
+    }
+
+    /**
+     * Create a result object.
+     *
+     * This method is called to create an actor to represent a search result.
+     *
+     * Implementations may return any `Clutter.Actor` to serve as the display
+     * result, or `null` for the default implementation.
+     *
+     * @param {ResultMeta} meta - A result metadata object
+     * @returns {Clutter.Actor} An actor for the result
+     */
+    createResultObject(meta) {
+        console.debug(`createResultObject(${meta.id})`);
+
+        return null;
+    }
+
+    /**
+     * Get result metadata.
+     *
+     * This method is called to get a `ResultMeta` for each identifier.
+     *
+     * @param {string[]} results - The result identifiers
+     * @param {Gio.Cancellable} [cancellable] - A cancellable for the operation
+     * @returns {Promise<ResultMeta[]>} A list of result metadata objects
+     */
+    getResultMetas(results, cancellable = null) {
+        console.debug(`getResultMetas([${results}])`);
+
+        const { scaleFactor } = St.ThemeContext.get_for_stage(global.stage);
+
+        return new Promise((resolve, reject) => {
+            const resultMetas = [];
+
+            for (let identifier of results) {
+                const meta = {
+                    id: identifier,
+                    name: 'Web Search',
+                    description: 'Launch web search',
+                    clipboardText: 'Launch web search',
+                    createIcon: (size) => {
+                        return new St.Icon({ gicon });
+                    },
+                };
+
+                resultMetas.push(meta);
+            }
+
+            resolve(resultMetas);
+        });
+    }
+
+    /**
+     * Initiate a new search.
+     *
+     * This method is called to start a new search and should return a list of
+     * unique identifiers for the results.
+     *
+     * @param {string[]} terms - The search terms
+     * @param {Gio.Cancellable} [cancellable] - A cancellable for the operation
+     * @returns {Promise<string[]>} A list of result identifiers
+     */
+    getInitialResultSet(terms, cancellable = null) {
+        console.debug(`getInitialResultSet([${terms}])`);
+
+        return new Promise((resolve, reject) => {
+            const identifiers = ['Web Search'];
+
+            resolve(identifiers);
+        });
+    }
+
+    /**
+     * Refine the current search.
+     *
+     * This method is called to refine the current search results with
+     * expanded terms and should return a subset of the original result set.
+     *
+     * Implementations may use this method to refine the search results more
+     * efficiently than running a new search, or simply pass the terms to the
+     * implementation of `getInitialResultSet()`.
+     *
+     * @param {string[]} results - The original result set
+     * @param {string[]} terms - The search terms
+     * @param {Gio.Cancellable} [cancellable] - A cancellable for the operation
+     * @returns {Promise<string[]>}
+     */
+    getSubsearchResultSet(results, terms, cancellable = null) {
+        console.debug(`getSubsearchResultSet([${results}], [${terms}])`);
+
+        return this.getInitialResultSet(terms, cancellable);
+    }
+
+    /**
+     * Filter the current search.
+     *
+     * This method is called to truncate the number of search results.
+     *
+     * Implementations may use their own criteria for discarding results, or
+     * simply return the first n-items.
+     *
+     * @param {string[]} results - The original result set
+     * @param {number} maxResults - The maximum amount of results
+     * @returns {string[]} The filtered results
+     */
+    filterResults(results, maxResults) {
+        console.debug(`filterResults([${results}], ${maxResults})`);
+
+        if (results.length <= maxResults)
+            return results;
+
+        return results.slice(0, maxResults);
     }
 }
 
-function init() {
+
+class Extension {
+    constructor() {
+        this._provider = null;
+    }
+
+    enable() {
+        if (this._provider === null) {
+            console.log('Enabling Quick Web Search 3');
+            this._provider = new SearchProvider();
+            SearchResults._registerProvider(this._provider);
+        }
+    }
+
+    disable() {
+        global.log('Desabling Quick Web Search 2');
+    //    if (this._provider instanceof SearchProvider) {
+     //       SearchResults._unregisterProvider(this._provider);
+      //      this._provider = null;
+       // }
+    }
+}
+
+function init(meta) {
     return new Extension();
 }
